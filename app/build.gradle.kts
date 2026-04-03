5642775e28743f39fcbc0b8c03bbdfc7ee0acbe9
lirons <liron.s@fire-arc.com>
2026-04-03 18:35:20 +0300

    Add full support for Arabic, Russian, French, Amharic, and Spanish languages

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.salino.sali"
         minSdk = 26
         targetSdk = 35
-        versionCode = 10
-        versionName = "1.0.9"
+        versionCode = 12
+        versionName = "1.1.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
